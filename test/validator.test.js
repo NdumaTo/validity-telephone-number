81c0574dfe9be57ca3e2f611c4f8c3a85fe34e0d
Tola Ndumari <tola.ndumari@clock.co.uk>
2020-01-30 14:45:15 +0000

    Adds tests

---
+++ test/validator.test.js
@@ -0,0 +1,83 @@
+const assert = require('assert')
+const validate = require('../index.js')
+
+describe('Telephone number validity', function() {
+  it('should allow valid phone numbers starting with 0', function() {
+    const fixture = [
+      'number',
+      'Telephone number',
+      { number: '01923261166' },
+      (err, message) => {
+        assert.deepEqual(err, null)
+        assert.deepEqual(message, null)
+      }
+    ]
+
+    validate(...fixture)
+  })
+
+  it('should allow valid phone numbers starting with a country code', function() {
+    const fixture = [
+      'number',
+      'Telephone number',
+      { number: '+441923261166' },
+      (err, message) => {
+        assert.deepEqual(err, null)
+        assert.deepEqual(message, null)
+      }
+    ]
+
+    validate(...fixture)
+  })
+
+  it('should not allow invalid phone numbers that are too short', function() {
+    const fixture = [
+      'number',
+      'Telephone number',
+      { number: '+4412345' },
+      (err, message) => {
+        assert.deepEqual(err, null)
+        assert.deepEqual(
+          message,
+          'Telephone number must be a valid telephone number'
+        )
+      }
+    ]
+
+    validate(...fixture)
+  })
+
+  it('should not allow invalid phone numbers that are too long', function() {
+    const fixture = [
+      'number',
+      'Telephone number',
+      { number: '+44012345678910' },
+      (err, message) => {
+        assert.deepEqual(err, null)
+        assert.deepEqual(
+          message,
+          'Telephone number must be a valid telephone number'
+        )
+      }
+    ]
+
+    validate(...fixture)
+  })
+
+  it('should not allow phone numbers containing text', function() {
+    const fixture = [
+      'number',
+      'Telephone number',
+      { number: '+44-Fake-Number' },
+      (err, message) => {
+        assert.deepEqual(err, null)
+        assert.deepEqual(
+          message,
+          'Telephone number must be a valid telephone number'
+        )
+      }
+    ]
+
+    validate(...fixture)
+  })
+})
