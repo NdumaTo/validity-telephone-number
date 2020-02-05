4827836a2c3ebe1e363eb19dc87935baf6d74683
Tola Ndumari <tola.ndumari@clock.co.uk>
2020-02-05 16:00:53 +0000

    fix(module): Changes module to es5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PhoneNumber = require('awesome-phonenumber')
+var PhoneNumber = require('awesome-phonenumber')
 
 module.exports = function phoneNumberValidator(
   key,
@@ -6,18 +6,18 @@ module.exports = function phoneNumberValidator(
   object,
   cb
 ) {
-  let value = object[key]
+  var value = object[key]
 
   if (typeof value !== 'string')
-    throw new TypeError(`${keyDisplayName} must be a string`)
+    throw new TypeError('keyDisplayName' + 'must be a string')
 
-  const assumedCountryCode = '+44'
+  var assumedCountryCode = '+44'
 
   if (value.startsWith('0')) value = assumedCountryCode + value
 
-  const validatePhoneNumber = new PhoneNumber(value)
+  var validatePhoneNumber = new PhoneNumber(value)
 
   if (validatePhoneNumber.isValid()) return cb(null)
 
-  cb(null, `${keyDisplayName} must be a valid telephone number`)
+  cb(null, 'keyDisplayName' + 'must be a valid telephone number')
 }
